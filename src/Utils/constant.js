fa19384082db5bece36554ae7095a8668d3b689e
KARTHIK <151136763+ssk9293@users.noreply.github.com>
2025-06-05 23:43:14 -0700

    Tittle change

--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -1,4 +1,4 @@
-export const logo = "https://images.ctfassets.net/y2ske730sjqp/6bhPChRFLRxc17sR8jgKbe/6fa1c6e6f37acdc97ff635cf16ba6fb3/Logos-Readability-Netflix-logo.png";
+export const logo = "./Smart.png";
 
 export const API_OPTIONS = {
   method: 'GET',
